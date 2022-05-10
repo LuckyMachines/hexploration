c20f4a5204f620d2ed954eaaa3ee17c3e117889d
Max Meier <max@studiomax.io>
2022-05-10 15:12:07 -0700

    tokens + wallet

--- contracts/tokens/DayNight.sol
+++ contracts/tokens/DayNight.sol
@@ -4,5 +4,10 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./GameToken.sol";
 
 contract DayNight is GameToken {
-    constructor() GameToken() {}
+    constructor(address controllerAddress) GameToken(controllerAddress) {
+        string[] memory tokenTypes = new string[](2);
+        tokenTypes[0] = "Day";
+        tokenTypes[1] = "Night";
+        addTokenTypes(tokenTypes);
+    }
 }
