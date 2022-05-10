ee413f77019cba0b4ee66349a9f48263e9991832
Max Meier <max@studiomax.io>
2022-05-10 12:01:33 -0700

    access control

--- contracts/HexplorationController.sol
+++ contracts/HexplorationController.sol
@@ -5,7 +5,30 @@ import "@luckymachines/game-core/contracts/src/v0.0/GameController.sol";
 import "./HexplorationBoard.sol";
 import "./HexplorationZone.sol";
 
-contract HexplorationController is GameController {
+contract HexplorationController is GameController, AccessControlEnumerable {
+    // functions are meant to be called directly by players by default
+    // we are adding the ability of a Controller Admin or Keeper to
+    // execute the game aspects not directly controlled by players
+    bytes32 public constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
+
+    // TODO:
+    // Connect to Chainlink VRF for random seeds when needed
     // submit move + space choice
     // use / swap item
+
+    modifier onlyAdminKeeper() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()) ||
+                hasRole(KEEPER_ROLE, _msgSender()),
+            "Admin or Keeper role required"
+        );
+        _;
+    }
+
+    function addKeeper(address keeperAddress)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        grantRole(KEEPER_ROLE, keeperAddress);
+    }
 }
--- contracts/decks/ItemDeck.sol
+++ contracts/decks/ItemDeck.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract ItemDeck {
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+contract ItemDeck is AccessControlEnumerable {
+    // This is an infinite deck, cards drawn are not removed from deck
+    // We can set card "quantities" for desireable probability
+
+    // controller role should be set to a controller contract
+    bytes32 public constant CONTROLLER_ROLE = keccak256("CONTROLLER_ROLE");
+
     constructor() {}
+
+    function drawCard(uint256 randomSeed)
+        public
+        virtual
+        onlyRole(CONTROLLER_ROLE)
+    {}
+
+    function getDeck() public view returns (string[] memory) {}
 }
