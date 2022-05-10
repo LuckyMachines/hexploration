f9b34d84070784f7abe330e5dfa1c53fb7348f7b
Max Meier <max@studiomax.io>
2022-05-10 16:51:21 -0700

    decks migrations

--- migrations/2_game_decks_migration.js
+++ migrations/2_game_decks_migration.js
@@ -1,5 +1,8 @@
-// Game Decks
 const EventDeck = artifacts.require("EventDeck");
+const AmbushDeck = artifacts.require("AmbushDeck");
+const ItemDeck = artifacts.require("ItemDeck");
+const LandDeck = artifacts.require("LandDeck");
+const TreasureDeck = artifacts.require("TreasureDeck");
 
 require("dotenv").config();
 
@@ -7,6 +10,34 @@ module.exports = async (deployer, network, [defaultAccount]) => {
   console.log("Deploying Event Deck");
   try {
     await deployer.deploy(EventDeck);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Ambush Deck");
+  try {
+    await deployer.deploy(AmbushDeck);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Item Deck");
+  try {
+    await deployer.deploy(ItemDeck);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Land Deck");
+  try {
+    await deployer.deploy(LandDeck);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Treasure Deck");
+  try {
+    await deployer.deploy(TreasureDeck);
   } catch (err) {
     console.error(err);
   }
