f7561500b13c08b27e47426b72a227a801eade91
Max Meier <max@studiomax.io>
2022-05-10 18:14:06 -0700

    placeholder controller address

--- migrations/3_game_tokens_migration.js
+++ migrations/3_game_tokens_migration.js
@@ -6,37 +6,39 @@ const ItemTokens = artifacts.require("Item");
 const PlayerStatusTokens = artifacts.require("PlayerStatus");
 
 module.exports = async (deployer, network, [defaultAccount]) => {
+  const HexplorationControllerAddress =
+    "0x0000000000000000000000000000000000000000";
   console.log("Deploying Day Night Tokens");
   try {
-    await deployer.deploy(DayNightTokens);
+    await deployer.deploy(DayNightTokens, HexplorationControllerAddress);
   } catch (err) {
     console.error(err);
   }
 
   console.log("Deploying Disaster Tokens");
   try {
-    await deployer.deploy(DisasterTokens);
+    await deployer.deploy(DisasterTokens, HexplorationControllerAddress);
   } catch (err) {
     console.error(err);
   }
 
   console.log("Deploying Enemy Tokens");
   try {
-    await deployer.deploy(EnemyTokens);
+    await deployer.deploy(EnemyTokens, HexplorationControllerAddress);
   } catch (err) {
     console.error(err);
   }
 
   console.log("Deploying Item Tokens");
   try {
-    await deployer.deploy(ItemTokens);
+    await deployer.deploy(ItemTokens, HexplorationControllerAddress);
   } catch (err) {
     console.error(err);
   }
 
   console.log("Deploying Player Status Tokens");
   try {
-    await deployer.deploy(PlayerStatusTokens);
+    await deployer.deploy(PlayerStatusTokens, HexplorationControllerAddress);
   } catch (err) {
     console.error(err);
   }
