63c940527867a3738e9534763adc37649d8ae9dc
Max Meier <max@studiomax.io>
2022-05-10 18:06:09 -0700

    token migration

--- migrations/3_game_tokens_migration.js
+++ migrations/3_game_tokens_migration.js
@@ -1,1 +1,43 @@
 // Game Tokens
+const DayNightTokens = artifacts.require("DayNight");
+const DisasterTokens = artifacts.require("Disaster");
+const EnemyTokens = artifacts.require("Enemy");
+const ItemTokens = artifacts.require("Item");
+const PlayerStatusTokens = artifacts.require("PlayerStatus");
+
+module.exports = async (deployer, network, [defaultAccount]) => {
+  console.log("Deploying Day Night Tokens");
+  try {
+    await deployer.deploy(DayNightTokens);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Disaster Tokens");
+  try {
+    await deployer.deploy(DisasterTokens);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Enemy Tokens");
+  try {
+    await deployer.deploy(EnemyTokens);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Item Tokens");
+  try {
+    await deployer.deploy(ItemTokens);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log("Deploying Player Status Tokens");
+  try {
+    await deployer.deploy(PlayerStatusTokens);
+  } catch (err) {
+    console.error(err);
+  }
+};
