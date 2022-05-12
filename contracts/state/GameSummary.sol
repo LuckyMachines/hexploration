3df931efefde85cad06c122766cf401a9e2c5c76
Max Meier <max@studiomax.io>
2022-05-11 18:26:16 -0700

    gc 0.0.7

---
+++ contracts/state/GameSummary.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+library GameSummary {
+    // player position
+    // ship position
+    // map tiles
+    // enemies
+    // tokens
+    //
+}
